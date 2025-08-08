5bbc5cdca8846b63ae8fa909d099410668bee008
ryan374538 <ryanodoyo087.com>
2025-08-08 21:12:41 +0300

    changed background color

--- connection.py
+++ connection.py
@@ -1,29 +1,29 @@
-#from sqlalchemy import create_engine
-#from sqlalchemy.orm import sessionmaker
-
-#username="root"
-#password="2480"
-#database="users"
-
-#path=f"mysql+pymysql://{username}:{password}@localhost/{database}"
-
-#engine=create_engine(path)
-
-#SessionLocal = sessionmaker(bind=engine)
-
-#session=SessionLocal()
-
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+username="root"
+password="2480"
+database="users"
+
+path=f"mysql+pymysql://{username}:{password}@localhost/{database}"
+
+engine=create_engine(path)
+
+SessionLocal = sessionmaker(bind=engine)
+
+session=SessionLocal()
+
+#import os
+#from sqlalchemy import create_engine
+#from sqlalchemy.orm import sessionmaker
+
 # Get the full database URL from an environment variable
-DATABASE_URL = os.getenv("DATABASE_URL")
+#DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Ensure URL uses psycopg2 driver if needed
-if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://", 1)
+#if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    #DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg2://", 1)
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-session = SessionLocal()
+#engine = create_engine(DATABASE_URL)
+#SessionLocal = sessionmaker(bind=engine)
+#session = SessionLocal()
